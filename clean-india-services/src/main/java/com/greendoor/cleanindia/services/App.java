5fbe475750a1ef1a0d89352dec933d3d443d680d
Bilahari Thanikkapambil <bilahari.th@gmail.com>
2014-09-28 17:32:20 +0530

    Initial Commit for clean India services
    Adding the very basic project skelton

---
+++ clean-india-services/src/main/java/com/greendoor/cleanindia/services/App.java
@@ -0,0 +1,13 @@
+package com.greendoor.cleanindia.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
